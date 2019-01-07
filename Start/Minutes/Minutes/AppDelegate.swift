f339e4d7412e643353649e12ffe6818933fdd935
Elangbam <jelangba@MGC000003524.chennai.ford.com>
2019-01-07 15:58:02 +0530

    Added Appcenter framework

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,11 @@
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterPush
+import AppCenterAnalytics
+
+//b9cdaa14-8196-4e66-bffd-fad2c31097de
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("b9cdaa14-8196-4e66-bffd-fad2c31097de",withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
