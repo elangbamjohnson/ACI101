56217efa80ac7067286612e4b71665a062f0f77a
Elangbam <jelangba@MGC000003524.chennai.ford.com>
2019-01-07 10:46:01 +0530

    Make the test fail

--- Start/Minutes/MinutesTests/MinutesTests.swift
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -7,27 +7,30 @@
 //
 
 import XCTest
+@testable import Minutes
+
 
 class MinutesTests: XCTestCase {
 
+    var entryUnderTest: Entry!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        entryUnderTest = Entry("Title", "Content")
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        entryUnderTest = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(entry?.title, "Titles", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
     }
 
 }
