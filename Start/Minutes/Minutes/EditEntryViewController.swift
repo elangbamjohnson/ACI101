31b132c3c71fde1513aee579de955a0efaf8cc7d
Elangbam <jelangba@MGC000003524.chennai.ford.com>
2019-01-08 12:35:39 +0530

    Removed crash

--- Start/Minutes/Minutes/EditEntryViewController.swift
+++ Start/Minutes/Minutes/EditEntryViewController.swift
@@ -1,6 +1,6 @@
 import Foundation
 import UIKit
-import AppCenterCrashes
+
 
 
 class EditEntryViewController: UIViewController
@@ -28,7 +28,7 @@ class EditEntryViewController: UIViewController
 
     @objc func onSave(_ sender: UIBarButtonItem)
     {
-        MSCrashes.generateTestCrash()
+        
         entry?.title = titleTextField.text!
         entry?.content = contentTextView.text!
         
