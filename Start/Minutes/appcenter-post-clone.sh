a96a47dff9e34d60f7d8dafdde9661a2eb2c1439
Elangbam <jelangba@MGC000003524.chennai.ford.com>
2019-01-08 14:19:36 +0530

    Test commit

--- Start/Minutes/appcenter-post-clone.sh
+++ Start/Minutes/appcenter-post-clone.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Example: Clone a required repository
-git clone https://github.com/elangbamjohnson/amazon-assessment.git
-
+# Make some changes
 # Example: Install App Center CLI
 npm install -g appcenter-cli
